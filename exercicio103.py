4965fe411d0e019872ff7dc028a6eacf87ab0841
DANILO VASCONCELOS <danilo.slima@hotmail.com>
2020-10-30 17:49:14 -0300

    exec103

---
+++ exercicio103.py
@@ -0,0 +1,4 @@
+#Faça um programa que tenha uma função chamada ficha(). que receba dois parametros
+#opcionais: o nome de um jogador e quantos gols ele (marcou)
+#o programada devera ser capaz de mostrar a ficha do jogador mesmo que algum dado
+#nao tenha sido informado corretamente
