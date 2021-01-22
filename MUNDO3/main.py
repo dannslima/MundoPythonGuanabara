aa5dd501b6eec482fe81b1621bfcb3bbcf5f74e7
DANILO VASCONCELOS <danilo.slima@hotmail.com>
2021-01-21 21:55:46 -0300

    aula01ok

--- MUNDO3/main.py
+++ MUNDO3/main.py
@@ -7,7 +7,7 @@ while True:
     itens['item']= str(input('Digite o nome do Item ')).upper()
     itens['quantidade']=int(input('Digite a quantidade deste item '))
     itens['Valor Unitário']= float(input('Digite o valor unitário do item '))
-    valortotal =  valortotal + itens['Valor Unitário']
+    valortotal = itens['Valor Unitário'] * itens['quantidade'] + valortotal
     itens['Peso'] = float(input('Digite o Peso do item KG '))
     carrinho.append(itens.copy())
     while True:
@@ -17,16 +17,31 @@ while True:
         print('Erro! Responda apenas S ou N')
     if resposta == 'N':
         break
+emails = list()
+while True:
+    emails.append(str(input('Digite o E-mail para dividir a conta')))
+    while True:
+        resposta = str(input('Deseja adcionar mais E-MAILS \n SIM [S] NÃO [N] ?')).upper()[0]
+        if resposta in 'SN':
+            break
+        print('Erro! Responda apenas S ou N')
+    if resposta == 'N':
+        break
+
 
 for i in carrinho:
     for k, v in i.items():
-        print (f' {k} __ {v}')
+        print (f' {k}: {v:.<15} ' , end='|')
 
-    print ('-'* 30)
+    print()
+print ('-'* 30)
 
 
-print(f' Houveram {len(carrinho)} produtos variados')
-print(f' O valor total do carrinho foi R${valortotal}')
+print(f' Produtos variados {len(carrinho)}')
+print(f' O valor total do carrinho foi R${valortotal:.2f}')
+print(f' Os e-mails digitados foram :')
+for i in emails:
+    print( i)
 
 
 
